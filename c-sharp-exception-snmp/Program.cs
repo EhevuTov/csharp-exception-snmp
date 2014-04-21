241958e99d66c92b36b2475ce8040b5c6b1354d7
James Gosnell <jamesgosnell@gmail.com>
2014-04-21 06:44:34 -0500

    INITIAL commit

---
+++ c-sharp-exception-snmp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Microsoft.Practices.Unity.Utility;
+
+namespace c_sharp_exception_snmp
+{
+    class Program
+    {
+        internal static IUnityContainer Container { get; private set; }
+        static void Main(string[] args)
+        {
+            Container = new UnityContainer().LoadConfiguration("snmptrapd");
+        }
+    }
+
+}

3b47f541a7f87a4bea4cca2843bcad4c44d6b1df
James Gosnell <jamesgosnell@gmail.com>
2014-04-21 07:23:20 -0500

    ADDED resolution calls

--- c-sharp-exception-snmp/Program.cs
+++ c-sharp-exception-snmp/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.Configuration;
 using Microsoft.Practices.Unity.Utility;
 
+using Lextm.SharpSnmpLib;
+using Lextm.SharpSnmpLib.Pipeline;
+using Lextm.SharpSnmpLib.Security;
+
 namespace c_sharp_exception_snmp
 {
     class Program
@@ -15,6 +19,20 @@ namespace c_sharp_exception_snmp
         static void Main(string[] args)
         {
             Container = new UnityContainer().LoadConfiguration("snmptrapd");
+            //HandlerMapping hMap = new HandlerMapping();
+            //Lextm.SharpSnmpLib.Pipeline.MessageHandlerFactory container = new Lextm.SharpSnmpLib.Pipeline.MessageHandlerFactory();
+            var users = Program.Container.Resolve<UserRegistry>();
+            var trapv2 = Container.Resolve<TrapV2MessageHandler>("TrapV2Handler");
+            /*
+            trapv2.MessageReceived += WatcherTrapV2Received;
+            using (var engine = Container.Resolve<SnmpEngine>())
+            {
+                engine.Listener.AddBinding(new IPEndPoint(IPAddress.Any, 162));
+                engine.Start();
+            }
+            ExceptionWrap e = new ExceptionWrap();
+            e.send();
+            */
         }
     }
 
